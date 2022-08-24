f8a37cc0e7df3a913e18cde9d64664d7b7b4e8f6
c.illies <c.illies@finatix.de>
2022-08-24 09:48:52 +0200

    implemented todos load effect

--- src/app/root-store/store-todo/actions/todo.actions.ts
+++ src/app/root-store/store-todo/actions/todo.actions.ts
@@ -2,14 +2,15 @@ import { createAction, props } from '@ngrx/store';
 
 import { Todo } from '../reducers/todo.reducer';
 
-export const loadTodos = createAction('[Todo] Load Todos');
+export const loadTodos = createAction('[Todo API] Load Todos');
+export const noop = createAction('[Todo State] No operation');
 
 export const loadTodosSuccess = createAction(
-  '[Todo] Load Todos Success',
+  '[Todo API] Load Todos Success',
   props<{ data: Todo[] }>()
 );
 
 export const loadTodosFailure = createAction(
-  '[Todo] Load Todos Failure',
+  '[Todo API] Load Todos Failure',
   props<{ error: any }>()
 );
--- src/app/root-store/store-todo/effects/todo.effects.ts
+++ src/app/root-store/store-todo/effects/todo.effects.ts
@@ -1,23 +1,40 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, InjectionToken } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { EMPTY, of } from 'rxjs';
-import { catchError, concatMap, map } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 
 import * as TodoActions from '../actions/todo.actions';
+
+const TODOS_KEY = 'todos';
+
+export const LOCAL_STORAGE_TOKEN = new InjectionToken<Storage>(
+  'localstorage ref',
+  {
+    factory: () => window.localStorage,
+  }
+);
 
 @Injectable()
 export class TodoEffects {
   loadTodos$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(TodoActions.loadTodos),
-      concatMap(() => {
-        return EMPTY.pipe(
-          map((data) => TodoActions.loadTodosSuccess({ data })),
-          catchError((error) => of(TodoActions.loadTodosFailure({ error })))
-        );
+      map(() => {
+        const localStorageData = this.localStorage.getItem(TODOS_KEY);
+        if (localStorageData) {
+          try {
+            const data = JSON.parse(localStorageData);
+            return TodoActions.loadTodosSuccess({ data });
+          } catch (error) {
+            return TodoActions.loadTodosFailure({ error });
+          }
+        }
+        return TodoActions.noop();
       })
     );
   });
 
-  constructor(private actions$: Actions) {}
+  constructor(
+    private readonly actions$: Actions,
+    @Inject(LOCAL_STORAGE_TOKEN) private readonly localStorage: Storage
+  ) {}
 }
